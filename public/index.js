2331fdac52cedd384a333a6aa026a1e187648f73
Anthony <anthonylsa12@gmail.com>
2024-01-05 17:48:24 -0800

    tried making fetch

--- public/index.js
+++ public/index.js
@@ -4,6 +4,7 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    const response = await('https://api.twelvedata.com/time_series?symbol=AAPL,EUR/USD,ETH/BTC:Huobi,TRP:TSX&interval=1min&apikey=fd42c2b747ae41c2b7eb1261bb1b3149')
 }
 
 main()
